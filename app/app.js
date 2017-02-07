9d2d068cead4744f3f7af31a78c01641e8f1c863
Pietro <pietro.gagliardi@sbb.ch>
2017-02-07 16:59:49 +0000

    add Parse & interface

--- app/app.js
+++ app/app.js
@@ -1,18 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  
+
   $routeProvider
   
     .when("/login", {
-        templateUrl : "pages/login.html"
+        templateUrl : "pages/login.html",
+        controller: "logCtrl",
     })
+    
     .when("/index", {
         templateUrl : "pages/index.html"
     })
---
+++ app/controller.js
@@ -0,0 +1,7 @@
+
+myApp.controller('logCtrl', ['$scope', function($scope) {
+    console.log('logCtrl');
+    Parse.initialize("asdvt6Fgkapis3dncjZdf"); 
+    Parse.serverURL = 'https://piegaserver.herokuapp.com//parse'
+    
+}]);
